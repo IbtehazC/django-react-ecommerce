5e4ad1d9a15efd1b2f4f3aa2ab24a5e023e2fedf
Ibtehaz Ali Chowdhury <ibtehaz.chowdhury@gmail.com>
2021-10-15 22:40:31 +0600

    First Commit

---
+++ ecom/api/views.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+
+# Create your views here.
+def home(requests):
+    return JsonResponse(
+        {
+            'info': 'Django',
+            'name': 'Hazz'
+        }
+    )
